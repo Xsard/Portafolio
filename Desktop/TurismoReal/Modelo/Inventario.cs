0405a35e1129df4365f09373157462e016a38a1b
Carlos Basaez <56442908+cxvetown@users.noreply.github.com>
2022-09-05 17:15:39 -0400

    creada clases serv_extra, inventario, objeto, mantencion

---
+++ Desktop/TurismoReal/Modelo/Inventario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Inventario
+    {
+        private int idInventario;
+        private int valorTotal;
+        private List<Objeto> objetos;
+        public int IdInventario { get => idInventario; set => idInventario = value; }
+        public int ValorTotal { get => valorTotal; set => valorTotal = value; }
+        public List<Objeto> Objetos { get => objetos; set => objetos = value; }
+    }
+}
---
+++ Desktop/TurismoReal/Modelo/Mantencion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Mantencion
+    {
+        private int idMantencion;
+        private Departamento departamento;
+        private string nombreMantenimiento;
+        private string descripcionMantenimiento;
+        private DateTime fechaInicio;
+        private DateTime fechaTermino;
+        private string estado;
+        private int costoMantencion;
+
+        public int IdMantencion { get => idMantencion; set => idMantencion = value; }
+        public Departamento Departamento { get => departamento; set => departamento = value; }
+        public string NombreMantenimiento { get => nombreMantenimiento; set => nombreMantenimiento = value; }
+        public string DescripcionMantenimiento { get => descripcionMantenimiento; set => descripcionMantenimiento = value; }
+        public DateTime FechaInicio { get => fechaInicio; set => fechaInicio = value; }
+        public DateTime FechaTermino { get => fechaTermino; set => fechaTermino = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public int CostoMantencion { get => costoMantencion; set => costoMantencion = value; }
+    }
+}
