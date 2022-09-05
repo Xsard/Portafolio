d55cad5905265c02f01969368906e1b9cfd55ae3
Ignacio Korenhof <65749633+Xsard@users.noreply.github.com>
2022-09-05 16:46:28 -0300

    Start up

--- Desktop/TurismoReal/Vista/Login.xaml.cs
+++ Desktop/TurismoReal/Vista/Login.xaml.cs
@@ -20,9 +20,9 @@ namespace Vista
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
         }
