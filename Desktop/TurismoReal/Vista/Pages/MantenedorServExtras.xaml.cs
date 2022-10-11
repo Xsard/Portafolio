4937e900e51ed6376f065b5ab259b651d429bd0f
Ierko <64447650+I-erko@users.noreply.github.com>
2022-10-10 23:40:46 -0300

    Validaciones preliminares ServExtra al actualizar

--- Desktop/TurismoReal/Vista/Pages/MantenedorServExtras.xaml.cs
+++ Desktop/TurismoReal/Vista/Pages/MantenedorServExtras.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using Controlador;
 using Modelo;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Vista.Pages
 {
@@ -27,6 +18,13 @@ namespace Vista.Pages
         {
             InitializeComponent();
             ListarSvE();
+        }
+        private void ItemError(object sender, ValidationErrorEventArgs e)
+        {
+            if (e.Action == ValidationErrorEventAction.Added)
+            {
+                MessageBox.Show(e.Error.ErrorContent.ToString());
+            }
         }
         #region Agregar
         private void btnAbrirAgregarServ_Click(object sender, RoutedEventArgs e)
