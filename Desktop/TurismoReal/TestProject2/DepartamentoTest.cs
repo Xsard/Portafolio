990b832491784e4d21f62c82f71f9d55c9b1943b
scacontreras <56443027+scacontreras@users.noreply.github.com>
2022-10-24 17:26:48 -0300

    carrera culia asquerosa

--- Desktop/TurismoReal/TestProject2/DepartamentoTest.cs
+++ Desktop/TurismoReal/TestProject2/DepartamentoTest.cs
@@ -5,6 +5,7 @@ namespace Pruebas
     public class DepartamentoTest
     {
         [Fact]
+        //Agregar departamento
         public void TestAgregarDepto()
         {
             //Arrange
@@ -25,6 +26,52 @@ namespace Pruebas
 
             //Act
             resObtenido = Controlador.CDepartamento.CrearDepto(departamento);
+
+            Assert.Equal(resEsperado, resObtenido);
+        }
+
+        [Fact]
+        //Actualizar departamento
+        public void TestActualizarDepto()
+        {
+            //Arrange
+            int resEsperado = 1;
+            int resObtenido;
+            Comuna comuna = new()
+            {
+                IdComuna = 1
+            };
+            Departamento departamento = new()
+            {
+                IdDepto = 65, 
+                TarifaDiara = 3000,
+                Direccion = "Avenida la tula",
+                NroDpto = 6,
+                Capacidad = 1,
+                Comuna = comuna
+            };
+
+            //Act
+            resObtenido = Controlador.CDepartamento.ActualizarDepto(departamento);
+
+            Assert.Equal(resEsperado, resObtenido);
+        }
+
+        //[Fact]
+        //Listar departamento
+        
+
+        [Fact]
+        //Eliminar departamento
+        public void TestEliminarDepto()
+        {
+            //Arrange
+            int resEsperado = 1;
+            int resObtenido;
+            Departamento departamento = new();
+
+            //Act   se usa el ID a eliminar
+            resObtenido = Controlador.CDepartamento.EliminarDpto(67);
 
             Assert.Equal(resEsperado, resObtenido);
         }
