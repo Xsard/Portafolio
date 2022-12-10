06cb6d10b6a33066511c5729728441fe0a49dcf7
Ignacio Korenhof <ig.korenhof@alumnos.duoc.cl>
2022-12-09 22:38:52 -0300

    Weas

---
+++ Desktop/TurismoReal/Vista/Pages/PotitoEmbrujado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vista.Pages
+{
+    internal class PotitoEmbrujado
+    {
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+        public int precio { get; set; }
+
+    }
+}
